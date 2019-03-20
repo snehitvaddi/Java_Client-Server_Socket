f43eec30dcb9c0143dc673866bcc661c2cc687d7
Snehit Vaddi <35267863+snehitvaddi@users.noreply.github.com>
2019-03-20 22:51:24 +0530

    Update MyClient.java

--- MyClient.java
+++ MyClient.java
@@ -6,18 +6,18 @@ public static void main(String[] args) {
 String a; 
 int count=0;
 try{      
-Socket s=new Socket("localhost",6666); 
+Socket s=new Socket("localhost",6666); //Creates socket for specified port number
 Scanner sc = new Scanner(System.in); 
 a = sc.nextLine(); 
-DataOutputStream out=new DataOutputStream(s.getOutputStream());  
+DataOutputStream out=new DataOutputStream(s.getOutputStream()); //DataOutputStream  
 out.writeUTF("C: "+a);  
 
-DataInputStream dis=new DataInputStream(s.getInputStream());  
+DataInputStream dis=new DataInputStream(s.getInputStream());  //DataInputStream
 String  str=(String)dis.readUTF();  
 System.out.println(str); 
-out.flush();  
-out.close(); 
-s.close();  
+out.flush();  //Buffer management
+out.close(); //output stream close
+s.close();  //socket close
 }catch(Exception e){System.out.println(e);}  
 }  
 }
