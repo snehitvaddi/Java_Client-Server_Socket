fc1efd11961ff7aa917b794a9b4eaba0b27d9490
Snehit Vaddi <35267863+snehitvaddi@users.noreply.github.com>
2019-03-20 22:49:42 +0530

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -4,26 +4,26 @@ public class MyServer {
 public static void main(String[] args){  
 try{  
 System.out.println("S: Server is started");
-ServerSocket ss=new ServerSocket(6666); 
+ServerSocket ss=new ServerSocket(6666); //Creates socket for specified port number
 
 System.out.println("S: Server is Waiting for client request"); 
-Socket s=ss.accept();//establishes connection
+Socket s=ss.accept(); //Establishes connection
   
 System.out.println("S: Client connected"); 
 
-DataInputStream in=new DataInputStream(s.getInputStream());  
+DataInputStream in=new DataInputStream(s.getInputStream());  //DataInputStream
 String  str=(String)in.readUTF();
 System.out.println(str);
 int intIndex = str.indexOf("hi");
    if(intIndex == - 1) {
 	System.out.println("pardon!!");} 
    else {
-        DataOutputStream out=new DataOutputStream(s.getOutputStream());  
+        DataOutputStream out=new DataOutputStream(s.getOutputStream());  //DataOutputStream
         out.writeUTF("S :Hello,I'm 5n How can I help you!!"); 
 	System.out.println("S :Hello,I'm 5n How can I help you!! ");
-        out.flush(); 
+        out.flush(); //Buffer Arrangement 
         }  
-ss.close();  
+ss.close();  //Socket Close
 }catch(Exception e){System.out.println(e);}  
 }  
 }  
